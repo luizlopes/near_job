3a7b928fbd2a2470dc786c264a91ed18ab670a3f
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-10-10 20:08:20 -0300

    refactoring best way service

--- app/services/best_way_service.rb
+++ app/services/best_way_service.rb
@@ -1,4 +1,5 @@
 class BestWayService
+  attr_reader :estimates_service
   def initialize(estimates_service = EstimatesService.new)
     @estimates_service = estimates_service
   end
@@ -12,23 +13,23 @@ class BestWayService
   private
 
   def distance(source, target)
-    estimate = @estimates_service.find(source, target)
+    estimate = estimates_service.find(source, target)
     estimate.present? ? estimate[0].distance : -1
   end
 
   def calculate_distances_from(source)
-    while @estimates_service.estimates_opened? source
-      @estimates_service.by_location(source).select(&:opened?)
-                        .sort_by(&:distance)
-                        .each do |estimate|
-                          step_down(source, estimate)
-                          estimate.close!
-                        end
+    while estimates_service.estimates_opened? source
+      estimates_service.by_location(source).select(&:opened?)
+                       .sort_by(&:distance)
+                       .each do |estimate|
+                         step_down(source, estimate)
+                         estimate.close!
+                       end
     end
   end
 
   def step_down(source, level_zero)
-    estimates = @estimates_service.by_location(level_zero.target)
+    estimates = estimates_service.by_location(level_zero.target)
     level_one_estimates = estimates.reject { |e| e.target.eql? source }
     level_one_estimates.each do |level_one|
       recalculate(source, level_zero, level_one)
@@ -37,12 +38,12 @@ class BestWayService
 
   def recalculate(source, level_zero, level_one)
     distance = level_zero.distance + level_one.distance
-    estimate = @estimates_service.find(source, level_one.target)
+    estimate = estimates_service.find(source, level_one.target)
     if estimate.present?
       change_estimate(estimate[0], distance, level_zero.target)
     else
-      @estimates_service.add_new(source, level_one.target, level_zero.target,
-                                 distance)
+      estimates_service.add_new(source, level_one.target, level_zero.target,
+                                distance)
     end
   end
 
--- spec/services/best_way_service_spec.rb
+++ spec/services/best_way_service_spec.rb
@@ -1,100 +1,102 @@
 require 'rails_helper'
 
 describe BestWayService do
+  let(:estimates_service) { EstimatesService.new }
+
   it 'from location to it self' do
-    best_way = BestWayService.new EstimatesService.new
+    best_way = BestWayService.new(estimates_service)
+
     distance = best_way.find_lower_distance('A', 'A')
     expect(distance).to eq 0
   end
 
-  it 'from location to another directly linked' do
-    estimates_service = EstimatesService.new
-    estimates_service.add 'A', 'B', 5
-    estimates_service.add 'B', 'A', 5
+  context 'when from location to another directly linked' do
+    before do
+      estimates_service.add 'A', 'B', 5
+      estimates_service.add 'B', 'A', 5
+    end
 
-    best_way = BestWayService.new estimates_service
-    distance = best_way.find_lower_distance('A', 'B')
-    expect(distance).to eq 5
+    it 'distance of between A and B should be 5' do
+      best_way = BestWayService.new estimates_service
+      distance = best_way.find_lower_distance('A', 'B')
+      expect(distance).to eq 5
+    end
   end
 
-  it 'from location to another directly linked (inverted)' do
-    estimates_service = EstimatesService.new
-    estimates_service.add 'A', 'B', 5
-    estimates_service.add 'B', 'A', 5
-    estimates_service.add 'B', 'C', 4
-    estimates_service.add 'C', 'B', 4
-    estimates_service.add 'A', 'C', 15
-    estimates_service.add 'C', 'A', 15
-    estimates_service.add 'C', 'D', 3
-    estimates_service.add 'D', 'C', 3
+  context 'when from location to another directly linked' do
+    before do
+      estimates_service.add 'A', 'B', 5
+      estimates_service.add 'B', 'A', 5
+      estimates_service.add 'B', 'C', 4
+      estimates_service.add 'C', 'B', 4
+      estimates_service.add 'A', 'C', 15
+      estimates_service.add 'C', 'A', 15
+      estimates_service.add 'C', 'D', 3
+      estimates_service.add 'D', 'C', 3
+    end
 
-    best_way = BestWayService.new estimates_service
-    distance = best_way.find_lower_distance('B', 'A')
-    expect(distance).to eq 5
+    it 'distance of between B and A should be 5' do
+      best_way = BestWayService.new estimates_service
+      distance = best_way.find_lower_distance('B', 'A')
+      expect(distance).to eq 5
+    end
   end
 
-  it 'from location to another second level linked' do
-    estimates_service = EstimatesService.new
-    estimates_service.add 'A', 'B', 5
-    estimates_service.add 'B', 'A', 5
-    estimates_service.add 'B', 'C', 4
-    estimates_service.add 'C', 'B', 4
-    estimates_service.add 'A', 'C', 15
-    estimates_service.add 'C', 'A', 15
-    estimates_service.add 'C', 'D', 3
-    estimates_service.add 'D', 'C', 3
+  context 'when from location to another with different ways' do
+    before do
+      estimates_service.add 'A', 'B', 5
+      estimates_service.add 'B', 'A', 5
+      estimates_service.add 'B', 'C', 4
+      estimates_service.add 'C', 'B', 4
+      estimates_service.add 'A', 'C', 15
+      estimates_service.add 'C', 'A', 15
+      estimates_service.add 'C', 'D', 3
+      estimates_service.add 'D', 'C', 3
+    end
 
-    best_way = BestWayService.new estimates_service
-    distance = best_way.find_lower_distance('A', 'C')
-    expect(distance).to eq 9
+    it 'distance of between A and C should be 9' do
+      best_way = BestWayService.new estimates_service
+      distance = best_way.find_lower_distance('A', 'C')
+      expect(distance).to eq 9
+    end
   end
 
-  it 'from location to another with different ways' do
-    estimates_service = EstimatesService.new
-    estimates_service.add 'A', 'B', 5
-    estimates_service.add 'B', 'A', 5
-    estimates_service.add 'B', 'C', 4
-    estimates_service.add 'C', 'B', 4
-    estimates_service.add 'A', 'C', 15
-    estimates_service.add 'C', 'A', 15
-    estimates_service.add 'C', 'D', 3
-    estimates_service.add 'D', 'C', 3
+  context 'when from location to another third level linked' do
+    before do
+      estimates_service.add 'A', 'B', 5
+      estimates_service.add 'B', 'A', 5
+      estimates_service.add 'B', 'C', 4
+      estimates_service.add 'C', 'B', 4
+      estimates_service.add 'A', 'C', 15
+      estimates_service.add 'C', 'A', 15
+      estimates_service.add 'C', 'D', 3
+      estimates_service.add 'D', 'C', 3
+    end
 
-    best_way = BestWayService.new estimates_service
-    distance = best_way.find_lower_distance('A', 'C')
-    expect(distance).to eq 9
+    it 'distance of between A and D should be 12' do
+      best_way = BestWayService.new estimates_service
+      distance = best_way.find_lower_distance('A', 'D')
+      expect(distance).to eq 12
+    end
   end
 
-  it 'from location to another third level linked' do
-    estimates_service = EstimatesService.new
-    estimates_service.add 'A', 'B', 5
-    estimates_service.add 'B', 'A', 5
-    estimates_service.add 'B', 'C', 4
-    estimates_service.add 'C', 'B', 4
-    estimates_service.add 'A', 'C', 15
-    estimates_service.add 'C', 'A', 15
-    estimates_service.add 'C', 'D', 3
-    estimates_service.add 'D', 'C', 3
+  context 'when from location to unreachable location' do
+    before do
+      estimates_service.add 'A', 'B', 5
+      estimates_service.add 'B', 'A', 5
+      estimates_service.add 'B', 'C', 4
+      estimates_service.add 'C', 'B', 4
+      estimates_service.add 'A', 'C', 15
+      estimates_service.add 'C', 'A', 15
+      estimates_service.add 'C', 'D', 3
+      estimates_service.add 'D', 'C', 3
+    end
 
-    best_way = BestWayService.new estimates_service
-    distance = best_way.find_lower_distance('A', 'D')
-    expect(distance).to eq 12
-  end
-
-  it 'from location to ponto inalcansavel' do
-    estimates_service = EstimatesService.new
-    estimates_service.add 'A', 'B', 5
-    estimates_service.add 'B', 'A', 5
-    estimates_service.add 'B', 'C', 4
-    estimates_service.add 'C', 'B', 4
-    estimates_service.add 'A', 'C', 15
-    estimates_service.add 'C', 'A', 15
-    estimates_service.add 'C', 'D', 3
-    estimates_service.add 'D', 'C', 3
-
-    best_way = BestWayService.new estimates_service
-    distance = best_way.find_lower_distance('A', 'H')
-    negative = -1
-    expect(distance).to eq negative
+    it 'distance of between A and H should be -1' do
+      best_way = BestWayService.new estimates_service
+      distance = best_way.find_lower_distance('A', 'H')
+      negative = -1
+      expect(distance).to eq negative
+    end
   end
 end
