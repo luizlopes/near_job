209471a31748b7e7982906bf30faec47382042f4
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-10-10 20:26:58 -0300

    refatoring job candidate service

--- app/services/job_candidate_service.rb
+++ app/services/job_candidate_service.rb
@@ -1,12 +1,14 @@
 class JobCandidateService
+  attr_reader :score_service
+
   def initialize(score_service = ScoreService.new)
     @score_service = score_service
   end
 
   def create(job_candidate_params)
     job_candidate = JobCandidate.new(job_candidate_params)
-    job_candidate.score = @score_service.call(job_candidate.job,
-                                              job_candidate.person)
+    job_candidate.score = score_service.call(job_candidate.job,
+                                             job_candidate.person)
     job_candidate
   end
 end
