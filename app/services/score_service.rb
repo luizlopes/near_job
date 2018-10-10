2c83377d5b47abace62353f0b6d41d6f022c30a8
Luiz Lopes <lg.oliveira.lopes@gmail.com>
2018-10-10 20:30:52 -0300

    refactoring score service

--- app/services/score_service.rb
+++ app/services/score_service.rb
@@ -1,4 +1,6 @@
 class ScoreService
+  attr_reader :level_factor_service, :distance_factor_service
+
   def initialize(level_factor_service = LevelFactorService,
                  distance_factor_service = DistanceFactorService.new)
     @level_factor_service = level_factor_service
@@ -6,9 +8,9 @@ class ScoreService
   end
 
   def call(job, person)
-    level_factor = @level_factor_service.call(job.level, person.level)
-    distance_factor = @distance_factor_service.call(job.localization,
-                                                    person.localization)
+    level_factor = level_factor_service.call(job.level, person.level)
+    distance_factor = distance_factor_service.call(job.localization,
+                                                   person.localization)
     (level_factor + distance_factor) / 2
   end
 end
